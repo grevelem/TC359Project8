ca315fc0066fc876e721956bcd0d4183e86f5e2d
Emily Grevel <grevelem@msu.edu>
2015-03-16 09:09:16 -0400

    time change

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -21,7 +21,7 @@ module MethodsPracticeHelpers
     # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
     # Time.now.strftime is a great method chain you can use for this purpose.
     # See how to use it at: http://apidock.com/ruby/Time/strftime
-    Time.now.strftime('%B %-d, %Y at %l:%M%P')
+    Time.now.strftime('%B %-d, %Y at %-l:%M%P')
   end
 
   # ------------------------------------------
